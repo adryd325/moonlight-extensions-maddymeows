82cbe0104a4aa978de5fca9802af40a7e93992c1
Maddy <maddy@fabricpaws.net>
2026-01-24 17:17:57 +0100

    "fix" selectivelyReduceMotion

--- src/selectivelyReduceMotion/index.ts
+++ src/selectivelyReduceMotion/index.ts
@@ -35,22 +35,6 @@ export const patches: ExtensionWebExports["patches"] = [
     },
   },
   {
-    find: '.dispatch({type:"POTIONS_TRIGGER_MESSAGE_CONFETTI"',
-    replace: {
-      match: /\i\.\i\.useReducedMotion/g,
-      replacement:
-        '$&||moonlight.getConfigOption("selectivelyReduceMotion","confetti")',
-    },
-  },
-  {
-    find: "Message must not be a thread starter message",
-    replace: {
-      match: /\[\i\.potioned]:/g,
-      replacement:
-        '"selectivelyReduceMotion-confetti":moonlight.getConfigOption("selectivelyReduceMotion","confetti"),$&',
-    },
-  },
-  {
     find: "isScrolledToTop:()=>",
     replace: {
       match: /(\i)\.to\(\{([^}]*)animate:([^}]*)}\)/,
